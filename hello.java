bbdc003e9edf2f2cd3db62c73190181a92402fde
Joshua <Joshuanalla.2@gmail.com>
2024-08-02 12:42:56 -0400

    first commit: java basics

---
+++ hello.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class hello
+{
+
+    public static void main (String arg[])
+    {
+        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("what is your name?");
+        String name = scanner.nextLine();
+        System.out.println("HI " + name + ", My name is junior");
+    
+    
+    
+    }
+}
+
+
