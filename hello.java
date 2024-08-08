4f21eeb5c53252271d2a09a56fc868333ec38f92
Joshua <Joshuanalla.2@gmail.com>
2024-08-08 15:35:21 -0400

    exception: throwing your own exception

--- hello.java
+++ hello.java
@@ -2,42 +2,29 @@ import java.util.*;
 
 //switch statement, remember the class has to be same as the file name class.java, public class class(parameters)
 
-// this time im accounting for exceptions, what if five is entered instead of '5'
+// here an exception is created and thrown back, either a generic exception or a exception class (later on)but as soon as 
+// the exception is found, the method terminates and throws trhe exception (throws exception line beneath method declaration)
+// back at the main function (the function that called it) so that it is dealt with in the main with the try-catch.
 
 public class hello {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.print("What number do you want: ");
-        int n = getint();
-        // added this exception so i can account for if a wrong input is entered at first
-        switch (n) {
-            case 1:
-                System.out.println("It's Monday!");
-                break;
-            case 2:
-                System.out.println("It's Tuesday");
-                break;
-            default:
-                System.out.println("Get your facts straight, cuh");
-                break;
-        }
+     try 
+     {
+        dosomething(true);
+     }  
+     catch(Exception e)
+     {
+        System.out.println("caught an exception");
+     }
     }
 
-    public static int getint ()
+    public static void dosomething (boolean t)
+    throws Exception
     {
-        while(true) // this loop will only stop when the return in the try block is successful
+        if(t == true)
         {
-            try{
-                return sc.nextInt(); // when this return statement wont work, it will trigger the catch mechanism to catch the exception
-            }
-            catch (Exception e) // this general exception catches all types of exceptions and the body will do the same thing 
-            // for any kind of exception
-            {
-                sc.next(); // this takes the input, that was not accepted, and then disposes of it? it just wont be a prob no more
-                System.out.println("whoops, pls enter a valid number yo"); // prints out the error msg and reruns the loop
-            }
+            throw new Exception();
         }
-    } 
+    }
 }
-
-// diff
