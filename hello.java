f8f2458450b0ea712a7dd38d28f40505fd0455fa
Joshua Nallaraja <joshuanallaraja@Joshuas-MacBook-Pro.local>
2024-08-14 14:14:26 -0400

    classes: with exceptions, constructors and member methods)

--- hello.java
+++ hello.java
@@ -1,30 +1,86 @@
 import java.util.*;
 
-//switch statement, remember the class has to be same as the file name class.java, public class class(parameters)
+// intro to object oriented programming
+// you create classes, blueprints, and when you call or use them, you create an object which is an instance of the class
+// classes have a couple aspects: methods (normal functions), constructors (a method called when an object is made), initializer (block-
+// of code that only happen once which is when the object is made).
 
-// here an exception is created and thrown back, either a generic exception or a exception class (later on)but as soon as 
-// the exception is found, the method terminates and throws trhe exception (throws exception line beneath method declaration)
-// back at the main function (the function that called it) so that it is dealt with in the main with the try-catch.
+// another thing is you cannot have two public classes in one file or it wont compile but the non-public class wont be available for use
+// elsewhere, you must only use it within that file. 
+// if you want to use it in multiple places then you must create a new .java file
 
-public class hello {
-    static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
-     try 
-     {
-        dosomething(true);
-     }  
-     catch(Exception e)
-     {
-        System.out.println("caught an exception");
-     }
+//public or private must be specifified (public means everyone can access it but privatet means only one person *you* can access it
+// getters and setters are methods used to access private members, such as a private variable. getters get that variable for you 
+// setters set it to whatever number you want
+
+// overloading methods are when two methods have the same name but different parameter types (note that return types and parameter 
+// names dont really mean anything here.
+
+// constructors are methods with no return type, same name as the class
+
+public class hello
+{
+    public static Scanner sc = new Scanner(System.in);
+    public static void main (String arg[])
+    {
+        something name = new something();
+        System.out.println("second class first variable: ");
+        int x = input();
+        System.out.println("second class second variable: ");
+        int y = input();
+        something name2 = new something(x,y);
+        System.out.println("first class instance, addition: " + name.addition());
+        System.out.println("first class, second method: multuplication: " + name.multiplication());
+        System.out.println("second class first method: addition: " + name2.addition());
+        System.out.println("Second class second method, multiplication: " + name2.multiplication());
+
     }
 
-    public static void dosomething (boolean t)
-    throws Exception
+    public static int input()
     {
-        if(t == true)
+        while (true)
         {
-            throw new Exception();
+            try
+            {
+                return sc.nextInt();
+            }
+            catch (Exception e)
+            {
+                sc.next();
+                System.out.println("wrong type amigo, try again: ");
+            }
         }
+    }
+
+}
+
+// also note when you are creating your own private class within a class folder, you are ineffect creating a new .class file.
+class something
+{
+    private int x;
+    private int y;
+    // constructor and default constructor
+    public something (int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public something ()
+    {
+        this.x = 1;
+        this.y = 2;
+    }
+
+    // methods for something like addition
+
+    public int addition()
+    {
+        return x + y;
+    }
+
+    public int multiplication()
+    {
+        return x * y;
     }
 }
